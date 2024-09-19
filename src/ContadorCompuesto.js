53aefa65c54ef4ce13c3a75099c4cf689bbc3906
ruben montani <ruben_montani@hotmail.com>
2024-09-19 11:36:38 -0300

    counter

---
+++ src/ContadorCompuesto.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import "./App.css"
+
+
+const getCalculo=(listanumeros)=>{
+    console.log("calculando")
+    return listanumeros.reduce((a,b)=>(a*b))
+}
+
+
+export const ContadorCompuesto = () => {
+ 
+
+  //estados
+   const numeros=[2,3,4,5,6,7,8,]
+   const[listanumeros,setListanumeros]=useState(numeros)
+
+    const [show,setShow]=useState(true)
+
+
+const agregarNumero=()=>{
+setListanumeros([...listanumeros, listanumeros[listanumeros.length -1]+1])
+console.log(listanumeros)
+}
+
+//button
+const onButton=()=>{
+    setShow(!show)
+}
+
+  
+
+    return (
+   <>
+   <h1>Numeros Compuestos y UseMemo</h1>
+   <h2>Resultado : </h2>
+   <p>{  getCalculo(listanumeros)}</p>
+   <button onClick={onButton}> {show ? "Show" : "Hide"}</button>
+   <button onClick={agregarNumero} >Agregar numero</button>
+   </>
+  )
+}
